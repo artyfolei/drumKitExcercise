60a6f7c4ffbb4f86327ee16600a2d21dc2020d09
Arty <artefolei@gmail.com>
2023-04-02 17:33:46 +0200

    playing sounds by pressing keys works

--- script.js
+++ script.js
@@ -7,51 +7,55 @@ for (let btnDrum of btnsDrum) {
     // drumSound.play();
     // alert("Kliknuls");
     let btnDrumInnerHTML = this.innerHTML;
-    switch (btnDrumInnerHTML) {
-      case "w":
-        const crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "a":
-        const kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-
-      case "s":
-        const snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-
-      case "d":
-        const tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case "j":
-        const tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case "k":
-        const tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case "l":
-        const tom4 = new Audio("sounds/tom-4.mp3");
-
-        tom4.play();
-        break;
-
-      default:
-        const defaultSound = new Audio("sounds/crash.mp3");
-        defaultSound.play();
-        break;
-    }
+    playSoundByLetter(btnDrumInnerHTML);
   });
 }
 
-// document.addEventListener("keydown", function () {
-//   alert("Key was pressed!");
-// });
+document.addEventListener("keydown", function (event) {
+  playSoundByLetter(event.key)
+});
+
+function playSoundByLetter(letter) {
+  switch (letter) {
+    case "w":
+      const crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "a":
+      const kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "s":
+      const snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "d":
+      const tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      const tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      const tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      const tom4 = new Audio("sounds/tom-4.mp3");
+
+      tom4.play();
+      break;
+
+    default:
+      const defaultSound = new Audio("sounds/crash.mp3");
+      defaultSound.play();
+      break;
+  }
+}
